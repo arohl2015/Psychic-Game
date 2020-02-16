c5aa2bb10821603ad7957eb278c5d888649ea7a3
A Rohl <april.rohl2015@gmail.com>
2020-02-16 10:04:40 -0700

    added in logic in js game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,14 +8,56 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var userGuesses = [];
+var round = 1;
 
 var userGuess = document.getElementById("guessed-letters");
 var guessLeft = document.getElementById("guesses-left");
 var win = document.getElementById("wins");
 var loss = document.getElementById("losses");
+var rounds = document.getElementById("round");
 
 var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+rounds.textContent = "Round: " + round;
+console.log(computerChoice);
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     userGuesses.push(event.key);
+    var guess = event.key.toLowerCase();
+    if (guess === computerChoice) {
+        wins++;
+        alert("You Won!");
+        displayResults();
+        reset()
+    }
+    else { 
+        if (guessesLeft === 1) {
+                losses++;
+                guessesLeft--;
+                alert("You Lost");
+                displayResults();
+                reset()
+    }   
+        else { guessesLeft--;
+            displayResults()
+            
+        }
+    }
+}
+
+
+function displayResults() {
+    win.innerHTML = wins;
+    loss.innerHTML = losses;
+    userGuess.innerHTML = userGuesses;
+    guessLeft.innerHTML = guessesLeft;
+}
+
+function reset ()   {
+    computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(computerChoice);
+    guessesLeft = 10;
+    userGuesses = [];
+    round++;
+    rounds.innerHTML = "Round: " + round;
+    displayResults();
 }
