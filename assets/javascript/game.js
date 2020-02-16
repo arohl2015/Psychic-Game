61817d34b3187d0c8eadfb75b10fabe8e4946a72
A Rohl <april.rohl2015@gmail.com>
2020-02-15 21:19:48 -0700

    updating js file to match html p tags

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,16 @@
 // start of variables
-var gameResults = {
-    wins: 0,
-    losses: 0,
-    guessesLeft: 8,
-    guessesTaken: [],
-    computerGuess: generator()
-}
+var computerGuesses = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+    "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var userGuesses = [];
+
+var userGuess = document.getElementById("guessed-letters");
+var guessLeft = document.getElementById("guesses-left");
+var win = document.getElementById("wins");
+var loss = document.getElementById("losses");
 
